536f949661794c382ba19cb3502588700d312cfb
joqk12345 <qianyanqk@163.com>
2017-08-28 16:57:27 +0800

    热词实体类添加注释

---
+++ src/main/java/com/horvan/trial/entity/HotWord.java
@@ -0,0 +1,130 @@
+package com.horvan.trial.entity;
+
+import com.horvan.trial.Constant;
+
+import javax.persistence.*;
+
+@Entity(name = Constant.TABLE_HOTWORD)
+public class HotWord {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private String number;
+
+    private String name;
+
+    private int type;
+
+    private int state;
+
+    private String message;
+
+    /**
+     * Gets id.  热词id
+     *
+     * @return the id
+     */
+    public long getId() {
+        return id;
+    }
+
+    /**
+     * Sets id.
+     *
+     * @param id the id
+     */
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    /**
+     * Gets number.
+     * 案号
+     * @return the number
+     */
+    public String getNumber() {
+        return number;
+    }
+
+    /**
+     * Sets number.
+     *
+     * @param number the number
+     */
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    /**
+     * Gets name. 热词名称
+     *
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Sets name.
+     *
+     * @param name the name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Gets type.
+     * 类型
+     * @return the type
+     */
+    public int getType() {
+        return type;
+    }
+
+    /**
+     * Sets type.
+     *
+     * @param type the type
+     */
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    /**
+     * Gets state.
+     * 状态
+     * @return the state
+     */
+    public int getState() {
+        return state;
+    }
+
+    /**
+     * Sets state.
+     *
+     * @param state the state
+     */
+    public void setState(int state) {
+        this.state = state;
+    }
+
+    /**
+     * Gets message.
+     * 备注
+     * @return the message
+     */
+    public String getMessage() {
+        return message;
+    }
+
+    /**
+     * Sets message.
+     *
+     * @param message the message
+     */
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
