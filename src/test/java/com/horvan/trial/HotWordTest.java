19e1a791dcbbb032cded08ecfcd1e5bd9cc026a5
joqk12345 <qianyanqk@163.com>
2017-08-29 10:52:20 +0800

    热词管理测试用例

---
+++ src/test/java/com/horvan/trial/HotWordTest.java
@@ -0,0 +1,26 @@
+package com.horvan.trial;
+
+import com.horvan.trial.entity.HotWord;
+import com.horvan.trial.repository.HotWordRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
+public class HotWordTest {
+
+    @Autowired
+    private HotWordRepository hotWordRepository;
+
+    @Test
+    public void addHotWord() throws  Exception{
+
+        hotWordRepository.save( new HotWord("003","庭审",1,2,"测试专用"));
+        hotWordRepository.save( new HotWord("004","布控",1,2,"测试专用"));
+    }
+
+
+}
